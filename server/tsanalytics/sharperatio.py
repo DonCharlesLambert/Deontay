4c27f4e1a1ea53868a12024c71298e99a97bfffb
DonCharlesLambert <epdonrap@gmail.com>
2024-02-10 15:15:32 +0000

    complete sharpe ratio analytic calculator

--- server/tsanalytics/sharperatio.py
+++ server/tsanalytics/sharperatio.py
@@ -4,5 +4,18 @@ class SharpeRatioCalculator(BaseAnalyticCalculator):
     def _getAnalyticName(self):
         return "Sharpe Ratio"
     
+    def _riskFreeRate(self):
+        # what is this
+        return 0.08
+
     def _run(self, equityCurve):
-        return -1
+        equitySeries = equityCurve['Equity']
+        intialEquity = equitySeries.iat[0]
+        finalEquity = equitySeries.iat[-1]
+        returnRatio =  ((finalEquity - intialEquity)/intialEquity)
+
+        dailyReturnSeries = equitySeries.pct_change()
+        meanDailyReturn = dailyReturnSeries.mean()
+        stdDailyReturn = dailyReturnSeries.std()
+        sharpeRatio = (returnRatio - self._riskFreeRate())/stdDailyReturn
+        return round(sharpeRatio, 2)
