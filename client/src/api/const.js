cd2d432ca911da7eb1a364748ddacec859407549
DonCharlesLambert <epdonrap@gmail.com>
2024-02-11 15:39:47 +0000

    adjust IP address to query, REMOVE CACHE DATA FROM REPO

--- client/src/api/const.js
+++ client/src/api/const.js
@@ -1,4 +1,4 @@
-const STRATEGIES_ENDPOINT = "http://localhost:8888/get-strategies"
-const ASSETS_ENDPOINT = "http://localhost:8888/get-assets"
+const STRATEGIES_ENDPOINT = "http://192.168.0.10:8888/get-strategies"
+const ASSETS_ENDPOINT = "http://192.168.0.10:8888/get-assets"
 
 export { ASSETS_ENDPOINT, STRATEGIES_ENDPOINT }
